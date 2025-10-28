30f907352174bc6ed323ed482d6841e36ed3f1e2
afadyukov <fadyukov.ak@gmail.com>
2025-10-28 20:51:41 +0300

    refactoring

--- ApplesGame/GameMain.cpp
+++ ApplesGame/GameMain.cpp
@@ -5,21 +5,21 @@
 #include <SFML/Audio.hpp>
 
 const std::string RESOURCES_PATH = "Resources/";
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+constexpr int SCREEN_WIDTH = 800;
+constexpr int SCREEN_HEIGHT = 600;
 
-const float INITIAL_SPEED = 100.f;
-const float ACCELERATION = 20.f;
+constexpr float INITIAL_SPEED = 100.f;
+constexpr float ACCELERATION = 20.f;
 
-const float PLAYER_SIZE = 20.f;
+constexpr float PLAYER_SIZE = 20.f;
 
-const int NUM_APPLES = 20;
-const float APPLE_SIZE = 20.f;
+constexpr int NUM_APPLES = 20;
+constexpr float APPLE_SIZE = 20.f;
 
-const int NUM_ROCKS = 3.f;
-const float ROCK_SIZE = 40.f;
+constexpr int NUM_ROCKS = 3.f;
+constexpr float ROCK_SIZE = 40.f;
 
-const float INITIAL_PAUSE_TIME = 3.f;
+constexpr float INITIAL_PAUSE_TIME = 3.f;
 
 enum class PlayerDirection
 {
